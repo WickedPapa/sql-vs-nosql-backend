cc488f8dc70fcc6b75abea20fc48264b2b4e08ae
skymontanop <pamontan@emeal.nttdata.com>
2026-03-21 21:48:53 +0100

    feat: UserMapperTest

--- src/test/java/it/montano/sqlvsnosql/user/service/UserPostgresServiceTest.java
+++ src/test/java/it/montano/sqlvsnosql/user/service/UserPostgresServiceTest.java
@@ -1,75 +1,69 @@
 package it.montano.sqlvsnosql.user.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 import it.montano.sqlvsnosql.common.mapper.UserMapper;
 import it.montano.sqlvsnosql.config.ConfiguredTest;
 import it.montano.sqlvsnosql.dto.UserRequest;
 import it.montano.sqlvsnosql.dto.UserResponse;
 import it.montano.sqlvsnosql.user.model.UserEntity;
 import it.montano.sqlvsnosql.user.repository.UserPostgresRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.instancio.junit.Given;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
 @ConfiguredTest
 class UserPostgresServiceTest {
 
-    @InjectMocks UserPostgresService service;
+  @InjectMocks UserPostgresService service;
 
-    @Mock UserMapper mapper;
-    @Mock
-    UserPostgresRepository repo;
+  @Mock UserMapper mapper;
+  @Mock UserPostgresRepository repo;
 
-    @Test
-    void createUser(@Given UserRequest request, @Given UserEntity entity, @Given UserResponse response) {
-        when(mapper.toEntity(request)).thenReturn(entity);
-        when(repo.save(entity)).thenReturn(entity);
-        when(mapper.toResponse(entity)).thenReturn(response);
+  @Test
+  void shouldCreateUser(
+      @Given UserRequest request, @Given UserEntity entity, @Given UserResponse response) {
+    when(mapper.toEntity(request)).thenReturn(entity);
+    when(repo.save(entity)).thenReturn(entity);
+    when(mapper.toResponse(entity)).thenReturn(response);
 
-        UserResponse result = service.createUser(request);
+    UserResponse result = service.createUser(request);
 
-        assertThat(result)
-                .isNotNull()
-                .isEqualTo(response);
-    }
+    assertThat(result).isNotNull().isEqualTo(response);
+  }
 
-    @Test
-    void deleteUser(@Given UUID userId) {
-        doNothing().when(repo).deleteById(userId);
+  @Test
+  void shouldDeleteUser(@Given UUID userId) {
+    doNothing().when(repo).deleteById(userId);
 
-        service.deleteUser(userId);
+    service.deleteUser(userId);
 
-        verify(repo).deleteById(userId);
-    }
+    verify(repo).deleteById(userId);
+  }
 
-    @Test
-    void getUserById(@Given UUID userId, @Given UserEntity entity, @Given UserResponse response) {
-        when(repo.findById(userId)).thenReturn(Optional.of(entity));
-        when(mapper.toResponse(entity)).thenReturn(response);
+  @Test
+  void shouldGetUserById(
+      @Given UUID userId, @Given UserEntity entity, @Given UserResponse response) {
+    when(repo.findById(userId)).thenReturn(Optional.of(entity));
+    when(mapper.toResponse(entity)).thenReturn(response);
 
-        UserResponse result = service.getUserById(userId);
+    UserResponse result = service.getUserById(userId);
 
-        assertThat(result)
-                .isNotNull()
-                .isEqualTo(response);
-    }
+    assertThat(result).isNotNull().isEqualTo(response);
+  }
 
-    @Test
-    void getUsers(@Given List<UserEntity> entities, @Given UserResponse response) {
-        when(repo.findAll()).thenReturn(entities);
-        when(mapper.toResponse(any(UserEntity.class))).thenReturn(response);
+  @Test
+  void shouldGetUsers(@Given List<UserEntity> entities, @Given UserResponse response) {
+    when(repo.findAll()).thenReturn(entities);
+    when(mapper.toResponse(any(UserEntity.class))).thenReturn(response);
 
-        List<UserResponse> result = service.getUsers();
+    List<UserResponse> result = service.getUsers();
 
-        assertThat(result)
-                .isNotNull()
-                .contains(response);
-    }
+    assertThat(result).isNotNull().contains(response);
+  }
 }
