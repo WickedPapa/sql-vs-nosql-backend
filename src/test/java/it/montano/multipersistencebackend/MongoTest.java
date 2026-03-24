ede5fb350a7fb977e48d7de333e86748564b4dcb
skymontanop <pamontan@emeal.nttdata.com>
2026-03-24 17:15:22 +0100

    feat: sonar

--- src/test/java/it/montano/multipersistencebackend/MongoTest.java
+++ src/test/java/it/montano/multipersistencebackend/MongoTest.java
@@ -1,11 +1,31 @@
 package it.montano.multipersistencebackend;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import it.montano.multipersistencebackend.order.service.OrderMongoService;
+import it.montano.multipersistencebackend.order.service.OrderService;
+import it.montano.multipersistencebackend.product.service.ProductMongoService;
+import it.montano.multipersistencebackend.product.service.ProductService;
+import it.montano.multipersistencebackend.user.service.UserMongoService;
+import it.montano.multipersistencebackend.user.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest(properties = {"app.datasource=MONGODB"})
 class MongoTest extends AbstractIntegrationTest {
 
+  @Autowired ApplicationContext context;
+
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertThat(context.getBean(UserService.class)).isNotNull().isInstanceOf(UserMongoService.class);
+    assertThat(context.getBean(ProductService.class))
+        .isNotNull()
+        .isInstanceOf(ProductMongoService.class);
+    assertThat(context.getBean(OrderService.class))
+        .isNotNull()
+        .isInstanceOf(OrderMongoService.class);
+  }
 }
--- src/test/java/it/montano/multipersistencebackend/PostgresTest.java
+++ src/test/java/it/montano/multipersistencebackend/PostgresTest.java
@@ -1,11 +1,33 @@
 package it.montano.multipersistencebackend;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import it.montano.multipersistencebackend.order.service.OrderPostgresService;
+import it.montano.multipersistencebackend.order.service.OrderService;
+import it.montano.multipersistencebackend.product.service.ProductPostgresService;
+import it.montano.multipersistencebackend.product.service.ProductService;
+import it.montano.multipersistencebackend.user.service.UserPostgresService;
+import it.montano.multipersistencebackend.user.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest(properties = {"app.datasource=POSTGRES"})
 class PostgresTest extends AbstractIntegrationTest {
 
+  @Autowired ApplicationContext context;
+
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertThat(context.getBean(UserService.class))
+        .isNotNull()
+        .isInstanceOf(UserPostgresService.class);
+    assertThat(context.getBean(ProductService.class))
+        .isNotNull()
+        .isInstanceOf(ProductPostgresService.class);
+    assertThat(context.getBean(OrderService.class))
+        .isNotNull()
+        .isInstanceOf(OrderPostgresService.class);
+  }
 }
